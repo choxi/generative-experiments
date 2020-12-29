3c5f0297bbbfa8351f8b65ea02997ff4fb01d6a6
Roshan Choxi <roshan.choxi@gmail.com>
2020-12-29 12:14:47 -0600

    bubbles

--- js/bubble-stroke.js
+++ js/bubble-stroke.js
@@ -3,12 +3,20 @@ console.log("Starting bubble-stroke")
 // Auto-reload the page
 // setInterval(() => { location.reload() }, 10000)
 
+var width = window.innerWidth
+var height = window.innerHeight
+var bubbles = []
+
 function setup() {
   width = window.innerWidth
   height = window.innerHeight
 
   frameRate(10)
   createCanvas(width, height)
+
+  for(let i = 0; i < 100; i++) {
+    bubbles.push(new Bubble())
+  }
 }
 
 function draw() {
@@ -19,4 +27,21 @@ function draw() {
   ellipse(1100, 200, 200, 200)
   // drawGradient(1100, 200, color(255, 255, 255, 4), color(255, 255, 255, 0), 600)
   Utils.radialGradient(1100, 200, 600, 600, color(255, 255, 255, 1), color(255, 255, 255, 0))
+
+  fill(255, 255, 255)
+  bubbles.forEach(bubble => {
+    ellipse(bubble.x, bubble.y, 10)
+    bubble.y -= 5
+
+    if (bubble.y <= 0) {
+      bubble.y = height
+    }
+  })
+}
+
+class Bubble {
+  constructor() {
+    this.x = random(width)
+    this.y = random(height)
+  }
 }

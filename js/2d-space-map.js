a3ea66ade7cd0628b518805d3db57886d01d6036
Roshan Choxi <roshan.choxi@gmail.com>
2021-01-14 22:48:49 -0600

    Tinkering with 2d-space-map

--- js/2d-space-map.js
+++ js/2d-space-map.js
@@ -38,31 +38,31 @@ class Planet {
       return
     }
 
-    let scale = 1000
+    let scale = 100000
 
     let { x, y, mass, velocity } = this
     let vector = new Vector((otherPlanet.x - x)*scale, (otherPlanet.y - y)*scale)
     let distance = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2))
-    let force = otherPlanet.mass * mass / Math.pow(distance, 2)
+    let force = otherPlanet.mass * mass / Math.pow(distance, 3)
     let acceleration = force / mass
     let newVelocity = new Vector(velocity.x + acceleration * vector.x, velocity.y + acceleration * vector.y)
     this.velocity = newVelocity
 
-    console.log(`force: ${force}`)
-    console.log(`velocity: (${newVelocity.x},${newVelocity.y})`)
+    // console.log(`force: ${force}`)
+    // console.log(`velocity: (${newVelocity.x},${newVelocity.y})`)
   }
 
   radius() {
-    return Math.sqrt(this.mass) / 5
+    return Math.sqrt(this.mass) / 50
   }
 }
 
 function randomPlanet() {
   let planet = new Planet()
-  planet.velocity = new Vector(random(-10, 10) / 10, random(-10, 10) / 10)
+  planet.velocity = new Vector(random(-20, 20) / 10, random(-20, 20) / 10)
   planet.x = random(- window.innerWidth / 2, window.innerWidth / 2)
   planet.y = random(- window.innerHeight / 2, window.innerHeight / 2)
-  planet.mass = random(200, 500)
+  planet.mass = random(200, 2000)
   planet.color = color(random(255), random(255), random(255))
   return planet
 }
@@ -73,33 +73,29 @@ var root
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   background(0);
-  frameRate(10)
+  frameRate(20)
   noStroke()
 
-  let planet = new Planet()
-  planet.mass = 4000
-  planet.fixed = true
-  planets[planet.id] = planet
+  // let planet = new Planet()
+  // planet.mass = 4000
+  // // planet.fixed = true
+  // planets[planet.id] = planet
 
-  let planetB = new Planet()
-  planetB.velocity = new Vector(1, 0)
-  planetB.x = -100
-  planetB.y = -200
-  planetB.mass = 400
-  planetB.color = color(0, 255, 0)
-  planets[planetB.id] = planetB
-
-  let planetC = new Planet()
-  planetC.velocity = new Vector(0.1, 1)
-  planetC.x = 400
-  planetC.y = -200
-  planetC.mass = 300
-  planetC.color = color(0, 0, 255)
-  planets[planetC.id] = planetC
-
-  for(let i = 0; i < 100; i++) {
+  for(let i = 0; i < 1000; i++) {
     let planet = randomPlanet()
     planets[planet.id] = planet
+  }
+}
+
+// Updates planet mass and returns planet to delete
+function collide(planetA, planetB) {
+  let biggerPlanet = planetA.radius() > planetB.radius() ? planetA : planetB
+  let smallerPlanet = planetA.radius() < planetB.radius() ? planetA : planetB
+  let collisionRadius = biggerPlanet.radius()
+  if (Math.abs(planetA.x - planetB.x) < collisionRadius &&
+      Math.abs(planetA.y - planetB.y) < collisionRadius) {
+    biggerPlanet.mass += smallerPlanet.mass
+    return smallerPlanet
   }
 }
 
@@ -116,14 +112,10 @@ function draw() {
         let otherPlanet = planets[otherID]
         planet.applyForceFrom(otherPlanet)
 
-        let biggerPlanet = planet.radius() > otherPlanet.radius() ? planet : otherPlanet
-        let smallerPlanet = planet.radius() < otherPlanet.radius() ? planet : otherPlanet
-        let collisionRadius = biggerPlanet.radius()
-        if (Math.abs(planet.x - otherPlanet.x) < collisionRadius &&
-            Math.abs(planet.y - otherPlanet.y) < collisionRadius) {
-          biggerPlanet.mass += smallerPlanet.mass
-          toDelete.push(smallerPlanet)
-        }
+        // let destroyed = collide(planet, otherPlanet)
+        // if (destroyed != null) {
+        //   toDelete.push(destroyed)
+        // }
       }
     })
 

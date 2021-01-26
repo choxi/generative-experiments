f22f8956616af5d4c3d2e94c4b85e3548aa27236
Roshan Choxi <roshan.choxi@gmail.com>
2021-01-25 21:47:08 -0600

    static tiles for performance

--- js/2001-tunnel.js
+++ js/2001-tunnel.js
@@ -10,12 +10,11 @@ let app = new p5(p => {
   let height = window.innerHeight
   let planeWidth = width * 2
   let planeHeight = height * 1.5
-  let tilt = Math.PI / 2 - 0.1
-  let blue = p.color(95, 212, 230)
-  let white = p.color(239, 233, 234)
   let circleCenter = new Vector(0, 100)
   let offset = 170
   let length = 100
+  var planeBuffer
+
   // let snakes = [-1/2, -1/4, -1/8, 3/8].map(rotFactor => {
   let snakes = [-7/8, -3/4, -5/8, -1/2, -3/8, -1/4, -1/8].map(rotFactor => {
     let rotation = rotFactor * Math.PI
@@ -27,6 +26,9 @@ let app = new p5(p => {
 
   p.setup = () => {
     p.createCanvas(width, height, p.WEBGL)
+    planeBuffer = p.createGraphics(width, height, p.WEBGL)
+    planeBuffer.scale(1, -1)
+    renderPlane(planeBuffer, planeWidth, planeHeight)
   }
 
   p.draw = () => {
@@ -44,35 +46,39 @@ let app = new p5(p => {
     snakes.forEach(s => s.render(p))
     p.pop()
 
-    // Plane
-    p.translate(0, 100, 0)
-
-    p.push()
-    p.fill(255, 0, 0)
-    p.rotateX(tilt)
-    p.plane(planeWidth, planeHeight)
-    p.pop()
-
-    let cellWidth = 20
-    let cellHeight = 20
-    let columns = planeWidth / cellWidth
-    let rows = planeHeight / cellHeight
-
-    for(let r=0; r < rows; r++) {
-      for(let c=0; c < columns; c++) {
-        let x = c*cellWidth
-        let y = r*cellHeight
-        p.push()
-        p.fill(p.lerpColor(blue, white, p.random(1, 10) / 10))
-        p.rotateX(tilt)
-        p.translate(- planeWidth / 2 + x + cellWidth/ 2, - planeHeight / 2 + y + cellHeight / 2, 1)
-        p.plane(cellWidth, cellHeight)
-        p.pop()
-      }
-    }
+    p.image(planeBuffer, - width / 2, - height / 2)
+    // renderPlane(p, planeWidth, planeHeight)
   }
 })
 
-function renderPlane(p) {
+function renderPlane(p, planeWidth, planeHeight) {
+  let tilt = Math.PI / 2 - 0.1
+  let blue = p.color(95, 212, 230)
+  let white = p.color(239, 233, 234)
+  let cellWidth = 20
+  let cellHeight = 20
+  let columns = planeWidth / cellWidth
+  let rows = planeHeight / cellHeight
 
+  p.noStroke()
+  p.translate(0, 100, 0)
+
+  p.push()
+  p.fill(255, 0, 0)
+  p.rotateX(tilt)
+  p.plane(planeWidth, planeHeight)
+  p.pop()
+
+  for(let r=0; r < rows; r++) {
+    for(let c=0; c < columns; c++) {
+      let x = c*cellWidth
+      let y = r*cellHeight
+      p.push()
+      p.fill(p.lerpColor(blue, white, p.random(1, 10) / 10))
+      p.rotateX(tilt)
+      p.translate(- planeWidth / 2 + x + cellWidth/ 2, - planeHeight / 2 + y + cellHeight / 2, 1)
+      p.plane(cellWidth, cellHeight)
+      p.pop()
+    }
+  }
 }
